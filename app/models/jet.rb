20d5c057c4913ecf2b1816af10118770dbe159d8
Nicolas Desbonnets <nicolas.desbonnets@hec.edu>
2019-03-06 15:01:46 +0100

    bugs fixed

--- app/models/jet.rb
+++ app/models/jet.rb
@@ -2,4 +2,12 @@ class Jet < ApplicationRecord
   belongs_to :user
   has_many :bookings
   mount_uploader :photo, PhotoUploader
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_day, presence: true
+  validates :pax, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :zipcode, presence: true
+  validates :photo, presence: true
 end
