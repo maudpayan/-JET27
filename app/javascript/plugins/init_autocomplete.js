e265f9faa5cd8952370100f964baefaca40442c5
Maud Payan <maudpayan@gmail.com>
2019-03-07 15:12:15 +0100

    map view + home research

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -7,12 +7,12 @@ const initAutocomplete = () => {
   }
 };
 
-const initAutocomplete2 = () => {
-  const addressInput = document.getElementById('home_address');
-  if (addressInput) {
-    places({ container: addressInput });
-  }
-};
+// const initAutocomplete2 = () => {
+//   const addressInput = document.getElementById('home_address');
+//   if (addressInput) {
+//     places({ container: addressInput });
+//   }
+// };
 
 export { initAutocomplete };
-export { initAutocomplete2 };
+// export { initAutocomplete2 };
